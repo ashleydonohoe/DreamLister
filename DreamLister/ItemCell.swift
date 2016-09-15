795e7c4a6e141485ef57422790ada3b514d84b34
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-09-14 22:36:32 -0400

    Basic setup of tableview

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Gabriele on 9/14/16.
+//  Copyright © 2016 Gabriele. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+}
