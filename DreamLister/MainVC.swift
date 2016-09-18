2e7badb756448062166e03021ef2ec7d07546c09
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-09-18 16:55:08 -0400

    added ability to add items to database from form

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -70,6 +70,8 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         
+        controller.delegate = self
+        
         self.controller = controller
         
         do {
